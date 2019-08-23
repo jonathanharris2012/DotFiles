29e3b4e7dfe1dd50bae6af5b96a2c9c81b8f5931
Jonathan Harris <jonathan.harris@sonicdrivein.com>
2019-08-23 08:43:02 -0500

    New function in install

--- install.ps1
+++ install.ps1
@@ -1,5 +1,14 @@
-Copy-Item -Path .\Microsoft.PowerShell_profile.ps1 $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -Force
-Copy-Item -Path .\Microsoft.PowerShell_profile.ps1 $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Force
+Function Create-IfMissing ($path) {
+    if (!(Test-Path $path)) {
+        $newFolder = Split-Path -Path $path
+        New-Item -ItemType 'Directory' -Path $newFolder
+    }
+    Copy-Item -Path .\Microsoft.PowerShell_profile.ps1 $path -Force
+}
+
+Create-IfMissing "$HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+Create-IfMissing "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+
 Copy-Item -Path .\.gitconfig $HOME\.gitconfig -Force
 Copy-Item -Path .\.gitbashrc $HOME\.bashrc -Force
 
@@ -11,3 +20,4 @@ if (Test-Path \\wsl$\Ubuntu-18.04\home\jonathanharris){
     Copy-Item -Path .\.gitconfig $HOME\wslLinux\.gitconfig -Force
     Copy-Item -Path .\.bashrc $HOME\wslLinux\.bashrc -Force
 }
+
